3d934f98f44a84db0a78f972556a0316afc8e22a
SaebomLee01 <84650643+SaebomLee01@users.noreply.github.com>
2021-05-23 12:35:29 +0900

    Add files via upload

---
+++ CameraMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Vector3 camOffset = new Vector3(0.0f, 0.0f, -5.0f);
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.TransformPoint(camOffset);
+        transform.LookAt(target);
+    }
+}
---
+++ PotionControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotionControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject hp = GameObject.Find("HPControl");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GameObject hp = GameObject.Find("HPControl");
+        if (other.gameObject.tag == "Player")
+        {
+            hp.GetComponent<HpController>().HpControl2();
+            Destroy(gameObject);
+        }
+    }
+}
