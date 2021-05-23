bd377ff4a2e134a2b6912f0f5433c07e9062a3d8
SaebomLee01 <84650643+SaebomLee01@users.noreply.github.com>
2021-05-23 11:55:17 +0900

    Add files via upload

---
+++ TrashController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrashController : MonoBehaviour
+{
+
+    public GameObject trash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject hp = GameObject.Find("HPControl");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject hp = GameObject.Find("HPControl");
+        if (other.gameObject.tag == "Player")
+        {
+            hp.GetComponent<HpController>().HpControl();
+            Destroy(gameObject);
+        }
+    }
+}
