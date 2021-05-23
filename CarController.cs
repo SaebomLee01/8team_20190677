f1daae6960cb035786ac4233ec3af8b2397b123c
SaebomLee01 <84650643+SaebomLee01@users.noreply.github.com>
2021-05-23 12:12:44 +0900

    Add files via upload

---
+++ CarController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CarController : MonoBehaviour
+{
+    float cMvSpeed = 30.0f;
+    float cRotSpeed = 150.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mv = Input.GetAxis("Vertical") * cMvSpeed * Time.deltaTime;
+        float rot = Input.GetAxis("Horizontal") * cRotSpeed * Time.deltaTime;
+
+        transform.Translate(0, 0, mv);
+        transform.Rotate(0, rot, 0);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            SceneManager.LoadScene("ClearScene");
+        }
+    }
+}
---
+++ HpController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HpController : MonoBehaviour
+{
+    GameObject hp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = GameObject.Find("HP");
+    }
+
+    public void HpControl()
+    {
+        hp.GetComponent<Image>().fillAmount -= 0.2f;
+        if (hp.GetComponent<Image>().fillAmount <= 0)
+        {
+            SceneManager.LoadScene("OverScene");
+        }
+    }
+
+    public void HpControl2()
+    {
+        hp.GetComponent<Image>().fillAmount += 0.1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
